6c9431c242d8dfe87eb702263b788e5f5e69a7db
Affan-Mamji <affanmamji8@gmail.com>
2024-08-06 03:13:51 -0700

    assinment-8

---
+++ assinment-8/main.dart
@@ -0,0 +1,26 @@
+void main() {
+  // if else
+  int hours = 24;
+  if (hours > 18) {
+    print("Good day");
+  } else {
+    print("not good day");
+  }
+
+  // list
+
+  List<String> justNames = ["done", "run", "fun"];
+
+  print("Names: $justNames");
+
+// map
+  var fruits = {'apple': 5, 'banana': 10, 'orange': 7};
+
+  print(fruits);
+
+  // int i = 5;
+
+  for (int i = 1; i <= 10; i++) {
+    print("3 * $i = ${3 * i}");
+  }
+}
